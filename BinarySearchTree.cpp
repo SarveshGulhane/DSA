f024a0614cc77ac396a5f5e09f3fe66703f6b2dd
Sarvesh Gulhane <49842473+SarveshGulhane@users.noreply.github.com>
2019-12-12 18:00:05 +0530

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,10 +4,38 @@ using namespace std;
 struct node
 {
 	char data;
-	struct node *left,*right;
+	struct node *left;
+	struct node *right;
 };
 
-node* Insert(node *root,int data)
+void preorder(struct node *root)
+{
+	if(root == NULL) return;
+
+	printf("%d ",root->data); 
+	preorder(root->left);     
+	preorder(root->right);    
+}
+
+void inorder(node *root)
+{
+	if(root == NULL) return;
+
+	inorder(root->left);       
+	printf("%d ",root->data);  
+	inorder(root->right);      
+}
+
+void postorder(node *root)
+{
+	if(root == NULL) return;
+
+	postorder(root->left);   
+	postorder(root->right);   
+	printf("%d ",root->data); 
+}
+
+node* insert(node *root,int data)
 {
 	if(root == NULL)
      {
@@ -16,9 +44,9 @@ node* Insert(node *root,int data)
 		root->left = root->right = NULL;
 	}
 	else if(data <= root->data)
-		root->left = Insert(root->left,data);
+		root->left = insert(root->left,data);
 	else 
-		root->right = Insert(root->right,data);
+		root->right = insert(root->right,data);
 	return root;
 }
 
@@ -32,7 +60,7 @@ int height(node* root)
         	return (1 + r);
 }
 
-void printGivenLevel(struct node* root, int level) 
+void printlevel(struct node* root, int level) 
 { 
 	if (root == NULL) 
         	return; 
@@ -40,18 +68,18 @@ void printGivenLevel(struct node* root, int level)
         	printf("%d ", root->data); 
     	else if (level > 1) 
     	{ 
-        	printGivenLevel(root->left, level-1); 
-        	printGivenLevel(root->right, level-1); 
+        	printlevel(root->left, level-1); 
+        	printlevel(root->right, level-1); 
     	} 
 } 
 
-void printLevelOrder(struct node* root) 
+void levelviz(struct node* root) 
 { 
     	int h = height(root); 
     	int i; 
     	for (i=1; i<=h; i++) 
     	{ 
-        	printGivenLevel(root, i); 
+        	printlevel(root, i); 
         	cout<<endl; 
     	} 
 } 
@@ -65,7 +93,7 @@ node* search(node* root,int num)
     	return search(root->left, num);
 } 
 
-node* minValueNode(node* root) 
+node* minvalue(node* root) 
 { 
     	node* current = root; 
     	while (current && current->left != NULL) 
@@ -73,14 +101,14 @@ node* minValueNode(node* root)
     	return current; 
 }
 
-node* deleteNode(node* root, int num1) 
+node* deleten(node* root, int num1) 
 { 
     	if (root == NULL) 
         	return root; 
     	if (num1 < root->data) 
-        	root->left = deleteNode(root->left, num1); 
+        	root->left = deleten(root->left, num1); 
     	else if (num1 > root->data) 
-        	root->right = deleteNode(root->right, num1); 
+        	root->right = deleten(root->right, num1); 
     	else
     	{ 
         	if (root->left == NULL) 
@@ -95,47 +123,75 @@ node* deleteNode(node* root, int num1)
             	delete root; 
             	return temp; 
         	} 
-        	node* temp = minValueNode(root->right); 
-        	root->data = temp->data; 
-        	root->right = deleteNode(root->right, temp->data); 
+        	node* temp = minvalue(root->right); 
+        	root->data = temp->data;
+        	root->right = deleten(root->right, temp->data); 
     	} 
     	return root; 
 }
-
+ 
 int main()
 {
 	node* root = NULL;
-     int keys[10],num,i;
+     int keys[100],num,n,op;
+
+	cout<<"Enter no. of Nodes: ";
+	cin>>n;
 
      cout<<"Enter keys:\n";
-     for(i=0;i<10;i++)
+     for(int i=0;i<n;i++)
      {
           cin>>keys[i];
      }
 
-	for (i=0;i<10;i++)
+	for (int k=0;k<n;k++)
      {
-		root = Insert(root, keys[i]);
+		root = insert(root, keys[k]);
      }
-	
-	cout<<"Level viz:\n"<<endl;
-	printLevelOrder(root);
 
-	
-	cout<<"\nEnter the element you want to search in the binary search tree\n";
-    	cin>>num;
-    	node* a=NULL;
-    	a=search(root,num);
-    	if(a!=NULL) 
-        	cout<<"The element is found\n";
-    	else
-        	cout<<"The element was not found\n";
+	do
+	{
+		cout<<"\nEnter Operation: 1.Pre/In/Post-order 2.Level viz 3.Search 4.Delete\n";
+		cin>>op;
 
-	
-	cout<<"\nEnter the data you want to delete: ";
-    	cin>>num;
-    	root=deleteNode(root,num);
-    	printLevelOrder(root);
+		switch (op)
+		{
+		case 1:
+			cout<<"\nPreorder: ";
+			preorder(root);
+			cout<<"\nInorder: ";
+			inorder(root);
+			cout<<"\nPostorder: ";
+			postorder(root);
+			cout<<endl;
+			break;
+		
+		case 2:
+			cout<<"\nLevel viz:\n";
+			levelviz(root);
+			break;
 
-    	return 0;
+		case 3:
+		{
+			cout<<"\nEnter the element you want to search: ";
+    			cin>>num;
+    			node* a=NULL;
+    			a=search(root,num);
+    			if(a!=NULL) 
+        			cout<<"The element is found\n";
+    			else
+        			cout<<"The element was not found\n";
+		}
+			break;
+
+		case 4:
+			cout<<"\nEnter the data you want to delete: ";
+    			cin>>num;
+    			root=deleten(root,num);
+    			levelviz(root);
+			break;
+		}
+	}while(op!=5);
+
+     return 0;
 }

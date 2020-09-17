81f3dbe792dec6b551f7b5da79d1bdc68a48ddbf
Sarvesh Gulhane <49842473+SarveshGulhane@users.noreply.github.com>
2020-09-17 08:03:29 +0530

    Create SelectionSort.go

---
+++ Go/SelectionSort.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func selectionSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		temp := arr[minIndex]
+		arr[minIndex] = arr[i]
+		arr[i] = temp
+	}
+
+}
+
+func printSorted(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d\t", arr[i])
+	}
+	fmt.Println()
+}
+
+func main() {
+	arr := []int{17, 55, 7, 28, 30, 3}
+	selectionSort(arr)
+	printSorted(arr)
+}

9bfef263945b658cc70a0f3547728a1945554611
Sarvesh Gulhane <49842473+SarveshGulhane@users.noreply.github.com>
2020-09-17 07:44:03 +0530

    Create BubbleSort.go

---
+++ Go/BubbleSort.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+}
+
+func printSorted(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d\t", arr[i])
+	}
+	fmt.Println()
+}
+
+func main() {
+	arr := []int{1, 13, 58, 36, 24, 15}
+	bubbleSort(arr)
+	printSorted(arr)
+}
